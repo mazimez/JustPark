c3310b2fe6c18b9ba4809221ec9d6d0d05b0a4f6
Asif Patel <asif987patel@gmail.com>
2021-04-11 17:35:21 +0530

    test comment

--- app/src/main/java/com/example/tillnow/main/Scan.java
+++ app/src/main/java/com/example/tillnow/main/Scan.java
@@ -43,6 +43,7 @@ import java.util.Objects;
 
 public class Scan extends AppCompatActivity {
 
+//    sdvsdvsdvsdvsvsdsdvsdv
     //local
     private String placeIdDB,placeIdScane;
     private String userId;
